eccf5a9fa61284c43bbd84f1ec418ad988216f11
Djsurry <djsurry@gmail.com>
2018-06-08 08:12:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,10 +21,13 @@ def main():
                 print('charging')
             else:
                 print('using battery')
+        if i == '-q':
+            quit()
     
 
 def updateList():
     global usage
+    charging = False
     while 1:
         cmd = ['pmset', '-g', 'batt']
         result = subprocess.run(cmd, stdout=subprocess.PIPE)
@@ -32,9 +35,13 @@ def updateList():
         l = s.split("'")
         status = l[1]
         if status == 'AC Power':
+            charging = True
+            print('-------CHARGING--------')
+        if status == 'Battery Power' and charging:
             now = datetime.datetime.now()
             usage.append(now.strftime("%m-%d-%Y %H:%M"))
-            print('-------CHARGING--------')
+            charging = False
+
         time.sleep(10)
 
     
@@ -43,3 +50,4 @@ if __name__ == "__main__":
     pilot = Thread(target=updateList)
     pilot.start()
     main()
+
